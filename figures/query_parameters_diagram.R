00c288f8d9d34a377353d9611c7f0047bde7222c
Liolia Bajemon <liolia.bajemon@ens-lyon.fr>
2025-10-07 16:32:32 +0200

    update script for figure

--- figures/query_parameters_diagram.R
+++ figures/query_parameters_diagram.R
@@ -40,3 +40,46 @@ digraph boxes_and_circles {
   {'rivername river', cityname} -> query [arrowhead = small]
   'rivername river' -> cityname [label = ' AND', color = white, fontname = Helvetica, fontsize = 8]
  } ")
+
+### EN FRANCAIS
+
+grViz("
+digraph boxes_and_circles {
+
+  graph [overlap = true]
+  
+  node [fontname = Helvetica,
+        fontsize = 10]
+        
+  subgraph {    
+  node [shape = rectangle,
+  color = cornflowerblue]
+  rank = same; rivière; ville; }
+
+  node [shape = rectangle,
+        fontname = Helvetica,
+        fontsize = 8,
+        color = thistle]
+  requête; pays; langue; 'domaine Google'; cookies; 'nombre de pages'; résultats
+
+  subgraph{
+  rank = same;
+  node [shape = oval, style = filled, fillcolor = orange, fontcolor = white, color = white] 
+  'Value Serp'
+  
+  node [shape = oval, style = filled, color = white, fillcolor = red, fontcolor = white]
+  Google }
+
+  'nombre de pages' -> 'Value Serp'
+  'requête'-> 'Value Serp' 
+  'pays'-> 'Value Serp' 
+  'langue'-> 'Value Serp' 
+  'cookies'-> 'Value Serp' 
+  'domaine Google'-> 'Value Serp' 
+  'Value Serp' -> Google [label = '     webscraping    ',
+                        fontname = Helvetica,
+                        fontsize = 8]
+  'Google' -> 'résultats'  
+  {rivière, ville} -> query [arrowhead = small]
+  rivière -> ville [label = ' AND', color = white, fontname = Helvetica, fontsize = 8]
+ } ")
